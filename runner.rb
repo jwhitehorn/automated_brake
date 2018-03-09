0bb874c2b9b132b1896ab6a06d760413b836d321
Jason Whitehorn <jason.whitehorn@gmail.com>
2018-03-08 19:49:02 -0600

    write to local tmp file during encoding & copy to final destination on completion

--- runner.rb
+++ runner.rb
@@ -2,12 +2,13 @@
 require 'yaml'
 
 directories = YAML.load_file 'settings.yml'
+command_base = "HandBrakeCLI -i $0/$1 -o /tmp/$1.m4v $3 && mv /tmp/$1.m4v $2/$1.m4v"
 
 loop do
   directories.each do |config|
     dir = config[:path]
     out_path = config[:out_path]
-    command = config[:command]
+    options = config[:options]
     Dir.entries(dir).each do |file|
       next if file[0] == "."[0]
       filename = "#{dir}/#{file}"
@@ -15,10 +16,10 @@ loop do
       sleep 30
       if first_size == File.size?(filename)
         #the file has not changed in size in 30 seconds, go ahead and begin processing it
-        task = command.gsub(/\$0/, dir).gsub(/\$1/, file).gsub(/\$2/, out_path)
+        task = command_base.gsub(/\$0/, dir).gsub(/\$1/, file).gsub(/\$2/, out_path).gsub(/\$3/, options)
         puts task
         puts system task
-        File.unlink(dir + "/" + file) if config[:delete]
+        File.unlink(filename) if config[:delete]
       end
     end
   end
